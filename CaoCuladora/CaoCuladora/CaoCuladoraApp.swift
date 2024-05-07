758cbea5aa9ec8c94f7e78cc01c6f777e51dbd11
João Vitor Lima Mergulhão <jvitormergulho@gmail.com>
2024-05-07 16:39:20 -0300

    create basic swift project

---
+++ CaoCuladora/CaoCuladora/CaoCuladoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaoCuladoraApp.swift
+//  CaoCuladora
+//
+//  Created by João Vitor Lima Mergulhão on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CaoCuladoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
